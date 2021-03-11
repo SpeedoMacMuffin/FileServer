618484e2ec1cb6e6985e5caaae4dddfeabcf2f6d
SpeedoMacMuffin <klarmann1@googlemail.com>
2021-03-11 09:31:45 +0000

    pi changes for review

--- server.js
+++ server.js
@@ -4,19 +4,22 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 4000;
 const fs = require("fs");
+const cors = require("cors");
 
+app.use(cors());
 app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.json({ message: "Hello World!" }));
 app.post("/upload", (req, res) => {
+
   if (req.files === null) {
     return res.status(400).json({ message: "No file uploaded" });
   }
   const file = req.files.file;
 
-  file.mv(`${__dirname}/uploads/${file.name}`, (err) => {
+  file.mv(`/home/pi/uploads/${file.name}`, (err) => {
     if (err) {
       console.error(err);
       return res.status(500).send(err);
@@ -26,7 +29,7 @@ app.post("/upload", (req, res) => {
 });
 app.get("/upload/:id", (req, res) => {
   const { id } = req.params;
-  const file = `${__dirname}/uploads/${id}`;
+  const file = `/home/pi/uploads/${id}`;
   fs.readFile(file, function (err, data) {
     res.write(data);
     return res.end();
@@ -34,7 +37,7 @@ app.get("/upload/:id", (req, res) => {
 });
 
 app.get("/local", (req, res) => {
-  const dir = `${__dirname}/uploads`;
+  const dir = `/home/pi/uploads`;
   try {
     const files = fs.readdirSync(dir);
     files.forEach((file) => {
@@ -52,7 +55,7 @@ app.get("/local", (req, res) => {
 });
 app.get("/local/:id", (req, res) => {
   const { id } = req.params;
-  const file = `${__dirname}/uploads/` + id;
+  const file = `/home/pi/uploads/` + id;
   const detail = fs.stat(file, (err, stats) => {
     if (err) {
       console.log(err);
@@ -69,8 +72,9 @@ app.get("/local/:id", (req, res) => {
 });
 app.get("/download/:id", (req, res) => {
   const { id } = req.params;
-  const file = `${__dirname}/uploads/` + id;
+  const file = `/home/pi/uploads/` + id;
   res.download(file);
+
 });
 
 app.listen(PORT, () =>
