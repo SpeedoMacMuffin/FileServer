0a4a19ba761eff612b9ea7353493ddc44d90cfc2
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-11 14:55:30 +0100

    added delete + delete-Controller

--- Routes/files.js
+++ Routes/files.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const filesController = require("../Controllers/files");
 
 router.get("/:id", filesController.getFileInfo);
+router.delete("/:id", filesController.deleteFile);
 router.get("/", filesController.getAllFiles);
 
 module.exports = router;
